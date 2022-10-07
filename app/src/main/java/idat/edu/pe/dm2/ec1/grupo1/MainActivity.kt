488c925b07d60cb96f5f49998b780ee649810404
RoggerK <at76324897@idat.edu.pe>
2022-10-06 20:32:18 -0500

    comienzo de git

---
+++ app/src/main/java/idat/edu/pe/dm2/ec1/grupo1/MainActivity.kt
@@ -0,0 +1,11 @@
+package idat.edu.pe.dm2.ec1.grupo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
