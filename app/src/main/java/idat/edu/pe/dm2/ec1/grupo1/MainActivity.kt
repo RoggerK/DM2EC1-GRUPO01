4355becf0e9e1941db8d099d1f7009c7c64bfd28
RoggerK <at76324897@idat.edu.pe>
2022-10-06 21:17:37 -0500

    calculo comun

--- app/src/main/java/idat/edu/pe/dm2/ec1/grupo1/MainActivity.kt
+++ app/src/main/java/idat/edu/pe/dm2/ec1/grupo1/MainActivity.kt
@@ -2,14 +2,28 @@ package idat.edu.pe.dm2.ec1.grupo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import idat.edu.pe.dm2.ec1.grupo1.databinding.ActivityMainBinding
+import kotlin.math.cos
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onClick(v: View?) {
+        calcularEstacionamiento()
+    }
+
+    private fun calcularEstacionamiento() {
+        val costo = 1500
+        var hora = binding.txtHoras.text.toString().toInt()
+        var minuto = binding.txtMinutos.text.toString().toInt()
+        var resultado = (hora * costo) + (minuto * (costo / 60))
+        binding.txvResultado.text = resultado.toString()
     }
 }
