7814ba529c2fe2612165926a030ddeecfee9c66a
RoggerK <at76324897@idat.edu.pe>
2022-10-06 21:31:58 -0500

    fin ec1-grupo1

--- app/src/main/java/idat/edu/pe/dm2/ec1/grupo1/MainActivity.kt
+++ app/src/main/java/idat/edu/pe/dm2/ec1/grupo1/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnCalcular.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -23,7 +24,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val costo = 1500
         var hora = binding.txtHoras.text.toString().toInt()
         var minuto = binding.txtMinutos.text.toString().toInt()
-        var resultado = (hora * costo) + (minuto * (costo / 60))
-        binding.txvResultado.text = resultado.toString()
+
+        if(minuto > 0) {
+            hora++
+        }
+
+        var operacion = (hora * costo) //+ (minuto * (costo / 60))
+        var resultado = operacion.toString()
+        binding.txvResultado.text = "Resultado: $resultado"
     }
 }
